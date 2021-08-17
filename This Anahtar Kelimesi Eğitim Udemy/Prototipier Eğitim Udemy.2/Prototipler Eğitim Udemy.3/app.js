6def326520a08ac1446db32f8a784db24d709989
halil6767 <halilibrahimdicle6@gmail.com>
2021-08-17 16:12:13 +0300

    Udemy Eğitim 4. Prototipler

---
+++ This Anahtar Kelimesi Eğitim Udemy/Prototipier Eğitim Udemy.2/Prototipler Eğitim Udemy.3/app.js
@@ -0,0 +1,17 @@
+function Employee(name,age){
+    this.name = name;
+    this.age = age;
+}
+Employee.prototype.showInfos = function(){
+    console.log("İsim: " + this.name + "Yaş: " + this.age); 
+}
+
+
+const emp1 = new Employee("Halil",23);
+const emp2 = new Employee("Oğuz",26);
+
+emp2.showInfos();
+console.log(emp1);
+console.log(emp2);
+
+
