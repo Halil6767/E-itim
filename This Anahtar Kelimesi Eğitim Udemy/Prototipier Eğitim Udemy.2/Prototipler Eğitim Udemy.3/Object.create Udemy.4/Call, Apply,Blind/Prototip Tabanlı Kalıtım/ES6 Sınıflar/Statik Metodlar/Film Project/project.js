da8a5c02a5de9b79a4078def9750dc46ce2f2ca8
halil6767 <halilibrahimdicle6@gmail.com>
2021-08-23 10:33:47 +0300

    deneme

---
+++ This Anahtar Kelimesi Eğitim Udemy/Prototipier Eğitim Udemy.2/Prototipler Eğitim Udemy.3/Object.create Udemy.4/Call, Apply,Blind/Prototip Tabanlı Kalıtım/ES6 Sınıflar/Statik Metodlar/Film Project/project.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+// UI Objesini Başlatma
+const ui = new UI();
+
+
+// Tüm Eventleri Yükleme
+
+eventListener();
+
+function eventListener(){
+    form.addEventListener("submit",addFilm);
+}
+function addFilm(e){
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+
+    if(title === "" || director === "" || url === ""){
+        // Hata
+    }
+    else {
+        // Yeni Film 
+        const newFilm = new Film(title,director,url);
+
+        ui.addFilmToUI(newFilm); // Arayüze Film Ekleme
+    }
+
+
+
+
+
+    e.preventDefault();
+}
+
